4d6f5caec0a871691c18cf2e8c149ff598cbbf87
René van Mil <rvanmil@me.com>
2019-11-26 10:19:14 +0100

    simplify file structure

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const eslintrc = require('./eslintrc.json')
-
 module.exports = {
-	...eslintrc,
 	extends: [
-		require.resolve('eslint-config-airbnb-base')
-	]
+		'airbnb-base'
+	],
+	rules: {
+		'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+		'class-methods-use-this': 'off',
+		'comma-dangle': ['error', 'never'],
+		indent: ['error', 'tab', {
+			SwitchCase: 1,
+			VariableDeclarator: 1,
+			outerIIFEBody: 1,
+			FunctionDeclaration: {
+				parameters: 1,
+				body: 1
+			},
+			FunctionExpression: {
+				parameters: 1,
+				body: 1
+			}
+		}],
+		'max-len': 'off',
+		'no-new': 'off',
+		'no-param-reassign': ['error', { props: false }],
+		'no-tabs': 'off',
+		'no-underscore-dangle': ['error', { allow: ['_id'] }],
+		'object-curly-newline': 'off',
+		'prefer-destructuring': 'off',
+		semi: ['error', 'never']
+	}
 }
